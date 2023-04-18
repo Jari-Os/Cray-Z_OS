a50652fd418015894f7b1b7a680b06dc2a008be3
Jari-Os <130354412+Jari-Os@users.noreply.github.com>
2023-04-18 22:17:15 +0200

    Update archcrazy.sh

--- Cray-Z_OS/ArchJari/archcrazy.sh
+++ Cray-Z_OS/ArchJari/archcrazy.sh
@@ -26,7 +26,7 @@ echo -ne "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    ( arch-chroot /mnt $HOME/ArchJari/scripts/1-setup.sh )|& tee 1-setup.log
     if [[ ! $DESKTOP_ENV == server ]]; then
       ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchJari/scripts/2-user.sh )|& tee 2-user.log
     fi
